1e3199185e2c0bff46c5f6e5423e229855b789d4
johnmorgan99 <96289431+johnmorgan99@users.noreply.github.com>
2023-08-23 19:29:37 +0100

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x7c259819e891a664c969a7dd5aaec592ade1435e";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
